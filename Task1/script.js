85af14a3804822aa6dedc0cdd60a2c1000876743
sameerishaare <sameergupta4873@gmail.com>
2024-04-05 21:27:31 +0530

    code added 3

--- Task1/script.js
+++ Task1/script.js
@@ -8,26 +8,32 @@ let swiper = new Swiper(".mySwiper", {
   loop: true,
   slidesPerView: 1,
   on: {
-	slideChange: function () {
-	  // Remove fade-in class from all images
-	  var allImages = document.querySelectorAll('.swiper-slide img');
-	  allImages.forEach(function (img) {
+    slideChange: function () {
+      // Remove fade-in class from all images
+      var allImages = document.querySelectorAll(".swiper-slide img");
+      allImages.forEach(function (img) {
+        img.classList.remove("zoom-in");
+      });
+
+      var allRating = document.querySelectorAll(".rating");
+      allRating.forEach(function (rating) {
+        rating.classList.remove("zoom-in");
+      });
+
+      // Add fade-in class to the active slide's image
+      var activeSlideImage = this.slides[this.activeIndex].querySelector("img");
+      activeSlideImage.classList.add("zoom-in");
+      var activeRating = this.slides[this.activeIndex].querySelector(".rating");
+      activeRating.classList.add("zoom-in");
+
+      document.querySelectorAll(".slider-img").forEach((img) => {
 		img.classList.remove('zoom-in');
-	  });
-
-	  var allRating = document.querySelectorAll('.rating');
-	  allRating.forEach(function (rating) {
-		rating.classList.remove('zoom-in');
-	  }
-	  );
-
-	  // Add fade-in class to the active slide's image
-	  var activeSlideImage = this.slides[this.activeIndex].querySelector('img');
-	  activeSlideImage.classList.add('zoom-in');
-	  var activeRating = this.slides[this.activeIndex].querySelector('.rating');
-	  activeRating.classList.add('zoom-in');
-	}
-  }
+		// Trigger reflow to reset the animation
+		void img.offsetWidth;
+		img.classList.add('zoom-in');
+      });
+    },
+  },
 });
 swiper.autoplay.start();
 document.querySelector(".swiper-button-next").style.top = "80%";
@@ -42,4 +48,3 @@ document.querySelector(".swiper-button-prev").style.left = "40%";
 document.querySelector(".swiper-button-prev").style.color = "rgb(241, 91, 21)";
 // scale down button
 document.querySelector(".swiper-button-prev").style.transform = "scale(0.5)";
-
